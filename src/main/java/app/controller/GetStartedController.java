679f87be0b33bb227ee492bdc09625e01c8664e9
Eltaj-R <eltacrecebov@mail.ru>
2020-06-24 23:29:54 +0400

    problem with css image

--- src/main/java/app/controller/GetStartedController.java
+++ src/main/java/app/controller/GetStartedController.java
@@ -1,4 +1,15 @@
 package app.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/start")
 public class GetStartedController {
+
+    @GetMapping
+    public String handler(){
+        return "landing";
+    }
 }
--- src/main/java/app/controller/LoginController.java
+++ src/main/java/app/controller/LoginController.java
@@ -1,4 +1,17 @@
 package app.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
 public class LoginController {
+
+    @GetMapping
+    public String loginHandler(){
+        return "index";
+    }
+
+
 }
