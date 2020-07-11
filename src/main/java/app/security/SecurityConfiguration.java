6522257c8a6cc40c236dd31f2b9d998d4cc7e809
rzayev1213 <rzayev1213@gmail.com>
2020-07-11 16:08:04 +0400

    Mappings corrected

--- src/main/java/app/security/SecurityConfiguration.java
+++ src/main/java/app/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package app.security;
 
+import app.service.ZUserCreationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    public SecurityConfiguration(ZUserCreationService z){
+//        z.create();
+    }
+
     @Bean
     public static PasswordEncoder encoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
@@ -21,14 +26,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/login", "/css/**", "/images/**").permitAll()
+                .antMatchers("/login", "/css/**", "/images/**", "/js/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
                 .usernameParameter("email")
-                .defaultSuccessUrl("/main", true);
+                .defaultSuccessUrl("/start", true);
 
 
     }
