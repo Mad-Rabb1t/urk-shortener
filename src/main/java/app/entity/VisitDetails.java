d45a2ca2e92ee1f620093b6df52875c004c048e7
rzayev1213 <rzayev1213@gmail.com>
2020-07-21 18:01:30 +0400

    Classes added

---
+++ src/main/java/app/entity/VisitDetails.java
@@ -0,0 +1,17 @@
+package app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class VisitDetails {
+
+}
