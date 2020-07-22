35d9866f563189d34e8de4878a4da20a8108fa61
rzayev1213 <rzayev1213@gmail.com>
2020-07-22 13:20:51 +0400

    Changes imported

--- src/main/java/app/service/ZUserCreationService.java
+++ src/main/java/app/service/ZUserCreationService.java
@@ -21,26 +21,26 @@ public class ZUserCreationService {
         this.enc = enc;
     }
 
-    //Test user creation. Should be removed
-//    @Bean
-//    public void create() {
-//        // resets all the information in db each time application is started
-//        repo.deleteAll();
-//        verifierRepo.deleteAll();
-//        ZUser user1 = new ZUser();
-//        ZUser user2 = new ZUser();
-//        user1.email = "random_user@mail.ru";
-//        user1.username = "random_user";
-//        user1.hasBeenActivated = true;
-//        user1.password = enc.encode("22");
-//        user2.email = "r3xf0x@yandex.ru";
-//        user2.username = "Alex";
-//        user2.hasBeenActivated = true;
-//        user2.password = enc.encode("123");
-//        repo.saveAll(Arrays.asList(
-//                user1, user2
-//        ));
-//
-//
-//    }
+    //Test user creation.
+    @Bean
+    public void create() {
+        // resets all the information in db each time application is started
+        repo.deleteAll();
+        verifierRepo.deleteAll();
+        ZUser user1 = new ZUser();
+        ZUser user2 = new ZUser();
+        user1.email = "random_user@mail.ru";
+        user1.username = "random_user";
+        user1.hasBeenActivated = true;
+        user1.password = enc.encode("22");
+        user2.email = "r3xf0x@yandex.ru";
+        user2.username = "Alex";
+        user2.hasBeenActivated = true;
+        user2.password = enc.encode("123");
+        repo.saveAll(Arrays.asList(
+                user1, user2
+        ));
+
+
+    }
 }
